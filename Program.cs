d05ca134a9ca930dda7763fec8c9ec15fe149b1b
Willow <will09600@gmail.com>
2023-04-17 18:06:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using TwoMoons.Settings;
+
+namespace TwoMoons;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        try
+        {
+            // Log.Logger = new LoggerConfiguration()
+            //     .Enrich.FromLogContext()
+            //     .MinimumLevel.Debug()
+            //     .MinimumLevel.Override("Quartz", LogEventLevel.Information)
+            //     .WriteTo.Console()
+            //     .WriteTo.File("Logs/latest-.log", rollingInterval: RollingInterval.Day,
+            //         retainedFileCountLimit: 14)
+            //     .CreateLogger();
+
+            await BotVariables.Initialize();
+
+            var builder = WebApplication.CreateBuilder();
+
+            Console.Title = $"TwoMoons v.{BotVariables.Version}";
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            await app.RunAsync();
+        }
+        catch (Exception exception)
+        {
+            // Log.Fatal(exception, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            // Log.CloseAndFlush();
+        }
+    }
+}
---
+++ Settings/BotVariables.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace TwoMoons.Settings
+{
+    public class BotVariables
+    {
+        internal static bool IsDebug;
+        internal static string? Version;
+
+        public static async Task Initialize()
+        {
+            if (Debugger.IsAttached)
+            {
+                IsDebug = true;
+                Version = "dev-env";
+            }
+            else
+            {
+                using var httpClient = new HttpClient();
+                var s = await httpClient.GetStringAsync(
+                    "https://raw.githubusercontent.com/MoonieGZ/FelicityOne/main/CHANGELOG.md");
+                Version = s.Split("## [")[1].Split("]")[0];
+            }
+        }
+    }
+}
